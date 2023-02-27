c61ff7475c3b987aa8648669332f297b8ecc8a05
Daniel Burgess <danieljbburgess@hotmail.co.uk>
2023-02-27 20:24:03 +0000

    Got a codebase test working

--- chomeTestUnit.py
+++ chomeTestUnit.py
@@ -3,6 +3,24 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 import time
 
+def test_codebase_find_ticket():
+    browser = webdriver.Chrome()
+    browser.set_window_size(1482, 996)
+    browser.get('https://code.silhouettesoftware.com/login')
+    username = browser.find_element(By.ID,"username")
+    username.send_keys("dburgess@silhouettesoftware.com")
+    password = browser.find_element(By.NAME,"password")
+    password.send_keys("LaGqkqCQJV4F2CE" + Keys.RETURN)
+    browser.find_element(By.XPATH,"//*[text()='Your Projects']").click()
+    browser.find_element(By.XPATH, "//*[text()='Studio']").click()
+    browser.find_element(By.XPATH, "//*[text()='Tickets']").click()
+    search = browser.find_element(By.NAME,"query")
+    search.send_keys("21000" + Keys.RETURN)
+    browser.find_element(By.LINK_TEXT,"21000").click()
+    assert "Cake" in browser.title
+    browser.quit()
+
+
 def test_silhouetteamerica_signin():
     browser = webdriver.Chrome()
     browser.set_window_size(1920,1080)
--- main.py
+++ main.py
@@ -22,5 +22,30 @@ def confirm_yahoo_title(driver):
     time.sleep(5) # Let the user actually see something!
     driver.quit()
 
+def codebase_find_ticket(browser):
+    browser.set_window_size(1482, 996)
+    browser.get('https://code.silhouettesoftware.com/login')
+    username = browser.find_element(By.ID,"username")
+    username.send_keys("dburgess@silhouettesoftware.com")
+    password = browser.find_element(By.NAME,"password")
+    password.send_keys("LaGqkqCQJV4F2CE" + Keys.RETURN)
+    browser.implicitly_wait(10)
+    browser.find_element(By.XPATH,"//*[text()='Your Projects']").click()
+    browser.implicitly_wait(10)
+    browser.find_element(By.XPATH, "//*[text()='Studio']").click()
+    browser.implicitly_wait(10)
+    browser.find_element(By.XPATH, "//*[text()='Tickets']").click()
+    browser.implicitly_wait(10)
+    search = browser.find_element(By.NAME,"query")
+    search.send_keys("21000" + Keys.RETURN)
+    browser.implicitly_wait(10)
+    browser.find_element(By.LINK_TEXT,"21000").click()
+    browser.implicitly_wait(10)
+    assert "Cake" in browser.title
+    browser.quit()
+
+codebase_find_ticket(firefox)
+codebase_find_ticket(chrome)
+
 confirm_yahoo_title(firefox)
 confirm_yahoo_title(chrome)
