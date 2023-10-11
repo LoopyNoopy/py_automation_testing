5ac96f32e05a3126ff6570f1d3ab36eb57178e0b
Daniel Burgess <danieljbburgess@hotmail.co.uk>
2023-10-11 19:09:05 +0100

    Updated playground to install stuff for demo

--- playground.py
+++ playground.py
@@ -1,117 +1,43 @@
+import subprocess
+
 import os
-import subprocess
-import asyncio
-import subprocess
-from appium import webdriver
-import pyuac
 
-def find_java_version():
-    items = os.listdir("C:\\Program Files\\Java\\")
-    return items[0]
 
-def install_appium():
-    install_commands = [
-    "winget install OpenJS.NodeJS.LTS --accept-package-agreements --accept-source-agreements",
-    "winget install -e --id Oracle.JDK.19 --accept-package-agreements --accept-source-agreements",
-    "winget install -e --id Google.AndroidStudio --accept-package-agreements --accept-source-agreements",
-    "npm i -g appium@next",
-    "appium driver install uiautomator2"]
-    for command in install_commands:
+def install_requirements():
+    print("Assigning GitHub token")
+    os.environ['GH_TOKEN'] = "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"
+    my_value = os.environ.get('GH_TOKEN')
+    if my_value:
+        print(f"The value of GH_TOKEN is: {my_value}")
+    else:
+        print("GH_TOKEN is not set.")
+    os.environ['NODE_OPTIONS'] = '--tls-min-v1.2'
+    my_value = os.environ.get('NODE_OPTIONS')
+    if my_value:
+        print(f"The value of NODE_OPTIONS is: {my_value}")
+    else:
+        print("NODE_OPTIONS is not set.")
+
+    print("Instaling Node, Puppeteer for Chrome and Firefox")
+    install_puppeteer_commands = [
+        "winget install -e --id Oracle.JDK.19 --accept-package-agreements --accept-source-agreements",
+        "winget install OpenJS.NodeJS.LTS --accept-package-agreements --accept-source-agreements",
+        "winget install -e --id Opera.Opera",
+        "winget install -e --id Google.AndroidStudio --accept-package-agreements --accept-source-agreements",
+        "npm i -g appium@next",
+        "appium driver install uiautomator2"
+        "npm config set registry https://registry.npmjs.org/",
+        "npm install --global npm",
+        "npm install -g npx",
+        "npm i allure-commandline",
+        "npm i puppeteer",
+        "npm i @puppeteer/browsers",
+        "npx @puppeteer/browsers install firefox",
+        "npx @puppeteer/browsers install chrome",
+        "npx @puppeteer/browsers install chromedriver",
+        "pip install --upgrade pytest pytest-allure-adaptor allure-pytest"]
+    for command in install_puppeteer_commands:
         os.system(command)
 
-    subprocess.run(["start", "cmd", "/k", "appium"], shell=True)
 
-    #Set envrioment variable for JDK version installed
-    os.environ['JAVA_HOME'] = 'C:\\Program Files\\Java\\' + find_java_version()
-    my_value = os.environ.get('JAVA_HOME')
-    if my_value:
-        print(f"The value of JAVA_HOME is: {my_value}")
-    else:
-        print("JAVA_HOME is not set.")
-
-    os.environ['ANDROID_HOME'] = "C:\\Users\\"+os.getenv("USERNAME")+"\\AppData\\Local\\Android\\Sdk"
-    my_value = os.environ.get('ANDROID_HOME')
-    if my_value:
-        print(f"The value of ANDROID_HOME is: {my_value}")
-    else:
-        print("ANDROID_HOME is not set.")
-
-
-
-current_directory = os.getcwd()
-
-print("Current Working Directory:", current_directory)
-
-def install_android():
-
-    from pywinauto import application
-    import time
-
-    # Start the application (replace 'notepad.exe' with the target application)
-    app = application.Application(backend="uia").start('C:\Program Files\Android\Android Studio\\bin\studio64.exe', wait_for_idle=True)
-
-    # Print control identifiers to the console
-    time.sleep(5)
-    child_window = app.window(title="Android Studio Setup Wizard")
-    child_window.dump_tree()
-
-    import keyboard
-
-    # Simulate a keypress
-    keyboard.press_and_release('enter')
-    time.sleep(0.5)
-    keyboard.press_and_release('enter')
-    time.sleep(0.5)
-    keyboard.press_and_release('tab')
-    keyboard.press_and_release('tab')
-    keyboard.press_and_release('enter')
-    time.sleep(0.5)
-    keyboard.press_and_release('tab')
-    keyboard.press_and_release('tab')
-    keyboard.press_and_release('right')
-    time.sleep(0.5)
-    keyboard.press_and_release('tab')
-    keyboard.press_and_release('tab')
-    keyboard.press_and_release('enter')
-
-
-
-    # Optional: Add a delay to observe the output (in seconds)
-    import time
-
-    # Close the application
-    #app.kill()
-
-
-    from selenium import webdriver
-    from webdriver_manager.firefox import GeckoDriverManager
-
-
-def add_to_path(path):
-    current_path = os.environ.get("PATH", "")
-    new_path = f"{current_path}{os.pathsep}{path}"
-    os.environ["PATH"] = new_path
-
-add_to_path("C:\\Users\\" + os.getlogin() + "\\AppData\Local\Android\Sdk\platform-tools")
-add_to_path("C:\\Users\\" + os.getlogin() + "\\AppData\Local\Android\Sdk\cmdline-tools\latest\bin")
-add_to_path("C:\\Users\\" + os.getlogin() + "\\AppData\Local\Android\Sdk\emulator")
-
-# Set envrioment variable for JDK version installed
-os.environ['JAVA_HOME'] = 'C:\\Program Files\\Java\\' + find_java_version()
-my_value = os.environ.get('JAVA_HOME')
-if my_value:
-    print(f"The value of JAVA_HOME is: {my_value}")
-else:
-    print("JAVA_HOME is not set.")
-
-os.environ['ANDROID_HOME'] = "C:\\Users\\" + os.getenv("USERNAME") + "\\AppData\\Local\\Android\\Sdk"
-my_value = os.environ.get('ANDROID_HOME')
-if my_value:
-    print(f"The value of ANDROID_HOME is: {my_value}")
-else:
-    print("ANDROID_HOME is not set.")
-
-#ToDo - restart the terminal or make these commands work in a new one
-#ToDo - make this dynamic to the latest version THEN download version based on appium caps
-os.system('sdkmanager "platforms;android-34"')
-os.system('sdkmanager "system-images;android-34;google_apis;x86_64"')
+install_requirements()
